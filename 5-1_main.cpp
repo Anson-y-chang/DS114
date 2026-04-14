9c6856407d0be460e1df99f66ea46ce43061132d
Zhang Yu-Cheng <zhangyu-cheng@Zhangs-MacBook-Air.local>
2026-04-15 00:10:02 +0800

    update cpp

--- 5-1_main.cpp
+++ 5-1_main.cpp
@@ -7,17 +7,17 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n;
-  cin >> n;
+  int val;
+  cin >> val;
 
   ListNode *head = nullptr;
 
-  for (int i = 0; i < n; i++) {
-    int val;
-    cin >> val;
-
-    head->next = new ListNode(val);
-    head->val = val;
+  if (val) {
+    head = new ListNode(val);
+    while (cin >> val) {
+      head->next = new ListNode(val);
+      head = head->next;
+    }
   }
 
   ListNode *res = swapDancePairs(head);
