a0f02bb9cb23666a4cb9c392e4114b6f8f6b5a4e
Zhang Yu-Cheng <zhangyu-cheng@Zhangs-MacBook-Air.local>
2026-04-15 00:35:57 +0800

    update typedef

--- ds_q5.hpp
+++ ds_q5.hpp
@@ -3,11 +3,11 @@
 #ifndef DS_Q5_HPP
 #define DS_Q5_HPP
 
-struct ListNode {
+typedef struct ListNode {
   int val;
   struct ListNode *next;
   ListNode(int val) : val(val), next(nullptr) {}
-};
+} ListNode;
 
 ListNode *swapDancePairs(ListNode *head);
 
