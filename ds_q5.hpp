93478df3ca501d86828890463ab3a751b9c51dc9
Zhang Yu-Cheng <zhangyu-cheng@Zhangs-MacBook-Air.local>
2026-04-15 00:39:48 +0800

    remove typedef from cpp

--- ds_q5.hpp
+++ ds_q5.hpp
@@ -3,11 +3,11 @@
 #ifndef DS_Q5_HPP
 #define DS_Q5_HPP
 
-typedef struct ListNode {
+struct ListNode {
   int val;
   struct ListNode *next;
   ListNode(int val) : val(val), next(nullptr) {}
-} ListNode;
+};
 
 ListNode *swapDancePairs(ListNode *head);
 
