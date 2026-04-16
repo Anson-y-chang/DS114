4ed6520e5990404b9c305c0121fa0963e3aac254
Zhang Yu-Cheng <zhangyu-cheng@Zhangs-MacBook-Air.local>
2026-04-17 03:18:02 +0800

    update check address

--- 5-1_main.c
+++ 5-1_main.c
@@ -18,12 +18,23 @@ int main() {
   ListNode *curr = dummyHead;
   int val;
 
+  int count = 0;
   while (scanf("%d", &val) == 1) {
     ListNode *tmp = (ListNode *)malloc(sizeof(ListNode));
     tmp->val = val;
     tmp->next = NULL;
     curr->next = tmp;
     curr = curr->next;
+    count++;
+  }
+
+  ListNode *before_address[count];
+  int before_value[count];
+  ListNode *tmp = dummyHead->next;
+  for (int i = 0; i < count; i++) {
+    before_address[i] = tmp;
+    before_value[i] = tmp->val;
+    tmp = tmp->next;
   }
 
   head = dummyHead->next;
@@ -31,6 +42,38 @@ int main() {
   while (res) {
     printf("%d ", res->val);
     res = res->next;
+  }
+
+  // 驗證指標交換
+  int isSwap = 1;
+  for (int i = 0; i < count; i++) {
+    ListNode *addr = before_address[i];
+
+    // 確認這個位址還存在於結果中
+    int found = 0;
+    ListNode *node = res;
+    while (node) {
+      if (node == addr) {
+        found = 1;
+        break;
+      }
+      node = node->next;
+    }
+
+    if (!found) {
+      isSwap = 0;
+      break;
+    }
+
+    // 確認值沒有被竄改
+    if (addr->val != before_value[i]) {
+      isSwap = 0;
+      break;
+    }
+  }
+
+  if (!isSwap) {
+    return 0;
   }
 
   free(dummyHead);
--- 5-1_main.cpp
+++ 5-1_main.cpp
@@ -1,5 +1,7 @@
 #include "ds_q5.hpp"
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -16,7 +18,44 @@ int main() {
     curr = curr->next;
   }
 
+  vector<pair<ListNode *, int>> before;
+  ListNode *tmp = dummyHead->next;
+  while (tmp) {
+    before.push_back({tmp, tmp->val});
+    tmp = tmp->next;
+  }
+
   ListNode *res = swapDancePairs(dummyHead->next);
+
+  map<ListNode *, int> afterNodes;
+  ListNode *node = res;
+  while (node) {
+    afterNodes[node] = node->val;
+    node = node->next;
+  }
+
+  bool isSwap = true;
+
+  for (int i = 0; i < before.size(); i++) {
+    ListNode *addr = before[i].first;
+
+    // 先確認這個位址還存在於結果中
+    if (afterNodes.find(addr) == afterNodes.end()) {
+      isSwap = false;
+      break;
+    }
+
+    // 再比較值有沒有被改
+    if (before[i].second != afterNodes[addr]) {
+      isSwap = false;
+      break;
+    }
+  }
+
+  if (!isSwap) {
+    return 0;
+  }
+
   while (res) {
     cout << res->val << " ";
     res = res->next;
